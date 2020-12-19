13f83e6352c25c2364d042a20eb75e6f9f4c6013
Brian Morrison II <brian@brianmorrison.me>
2020-12-19 08:08:24 -0600

    Fixed bot options

--- src/types/BotOptions.ts
+++ src/types/BotOptions.ts
@@ -1,10 +1,10 @@
 import { Message } from "discord.js"
 import DiscordBotConfiguration from "./DiscordBotConfiguration"
-// import YouTubeBotConfiguration from "./YouTubeBotConfiguration"
+import YouTubeBotConfiguration from "./YouTubeBotConfiguration"
 
 type BotOptions = {
   discordBotConfig?: DiscordBotConfiguration;
-  // youtubeBotConfig?: YouTubeBotConfiguration;
+  youtubeBotConfig?: YouTubeBotConfiguration;
   allowBotToBotInteraction?: boolean;
   onReady?(message: Message): void;
 }
